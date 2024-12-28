2ead9e717694a69d84b9d3ffd93405741f2a781e
Arpan122 <aniruddha.arpan12@gmail.com>
2024-12-28 18:36:30 -0500

    Added timer to energy drink effect and drink respawn

--- akgamedev-project/scripts/energy_drink.gd
+++ akgamedev-project/scripts/energy_drink.gd
@@ -1,10 +1,27 @@
 extends Area2D
 
+var active: bool = true
+var timer: Timer
 
+func _ready() -> void:
+	timer = Timer.new()
+	timer.autostart = false
+	timer.one_shot = true
+	timer.wait_time = 3.0
+	add_child(timer)
 
+func _process(delta: float) -> void:
+	if (!active):
+		self.visible = false
+		if (timer.time_left <= 0):
+			active = true
+	else:
+		self.visible = true
 
 func _on_area_entered(area: Area2D) -> void:
-	area.get_parent().SPEED = 7
-	self.queue_free()
-	area.get_parent().is_energy_taken = true
-	print("Drugs activated")
+	if (area.name == "Player" and active):
+		active = false
+		area.get_parent().is_energy_taken = true
+		area.get_parent().timer.start()
+		self.timer.start()
+		print("Drugs activated")
--- akgamedev-project/scripts/player.gd
+++ akgamedev-project/scripts/player.gd
@@ -1,25 +1,20 @@
 extends CharacterBody2D
 
-var SPEED = 3.0
+var speed = 3.0
 var behind_locker = false
 var spaceOn = false	
 var is_energy_taken = false
+var energyLeft: int = 0
+var timeActivated: int
 var oldPos: Vector2
 var collisionInfo
-@export var locker_instructions: TextEdit	#Textbox for the instructions (TODO: change from using visible to manipulating the inner text)
-@export var mesh: MeshInstance2D
-@export var camera: Camera2D
+@onready var locker_instructions: TextEdit = $"TextEdit"	#Textbox for the instructions
+@onready var mesh: MeshInstance2D = $"MeshInstance2D"
+@onready var camera: Camera2D = $"Camera2D"
 @onready var spawnPoints: Node2D = $"../SpawnPoints"
+var timer: Timer
 var near_locker = false
 
-	# TAKE_INPUT function taken out because it only allowed one input at the time
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if(false): # not using this rn for development purposes
-		print(get_parent().name)
-		var sp = spawnPoints.get_children() 
-		var pointNum = int(randf() * len(sp)) #chooses a random point to start at
-		position = sp[pointNum].position
 
 func collidingLocker(currently: bool, collision=null): # this one is called by the area2d in player
 	collisionInfo = collision
@@ -34,14 +29,27 @@ func collidingLocker(currently: bool, collision=null): # this one is called by t
 		locker_instructions.visible = false
 	near_locker = currently
 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer = Timer.new()
+	timer.autostart = false
+	timer.one_shot = true
+	timer.wait_time = 2.0
+	add_child(timer)
+	if(false): # not using this rn for development purposes
+		print(get_parent().name)
+		var sp = spawnPoints.get_children() 
+		var pointNum = int(randf() * len(sp)) #chooses a random point to start at
+		position = sp[pointNum].position
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	#Movement input
 	if(!behind_locker):
-		position.x -= SPEED * int(Input.is_action_pressed("left"))
-		position.x += SPEED * int(Input.is_action_pressed("right"))
-		position.y -= SPEED * int(Input.is_action_pressed("up"))
-		position.y += SPEED * int(Input.is_action_pressed("down"))
+		position.x -= speed * int(Input.is_action_pressed("left"))
+		position.x += speed * int(Input.is_action_pressed("right"))
+		position.y -= speed * int(Input.is_action_pressed("up"))
+		position.y += speed * int(Input.is_action_pressed("down"))
 	
 	move_and_slide() # this is a command to apply physics
 	#Functionality for entering and exiting a locker
@@ -58,3 +66,11 @@ func _physics_process(_delta: float) -> void:
 		spaceOn = false
 		
 	mesh.visible = !behind_locker
+	
+	if (timer.time_left <= 0 and is_energy_taken):
+		is_energy_taken = false
+	
+	if (is_energy_taken):
+		self.speed = 6.0
+	else:
+		self.speed = 3.0
