7d845199d92ad7caf157047dd6756edf2bd638ea
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-22 15:48:48 -0500

    Patrol mode

--- akgamedev-project/scripts/teacher-pathfinder.gd
+++ akgamedev-project/scripts/teacher-pathfinder.gd
@@ -2,20 +2,38 @@ extends CharacterBody2D
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 @export var mode: int = 0 # 0 = patrol, 1 = chase 
+@export var patrolPoints: Array[Node2D]
+
+var current = 0
+var currentMax
+
+func _ready() -> void:
+	currentMax = patrolPoints.size() - 1
 
 func _physics_process(delta: float) -> void:
 	
 	var direction = Vector3()
 	
 	if mode == 0:
-		print(0)
+		nav.target_position = patrolPoints[current].global_position
+		if global_position.distance_to(patrolPoints[current].global_position) <= 10:
+			current+=1
+			print(current, ">", currentMax)
+			if current > currentMax:
+				print("above", patrolPoints.size())
+				current = 0
+		
+
+		
+		
+		
 	if mode == 1:
 		print(1)
 		
-	nav.target_position = get_global_mouse_position()
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
 	
-	velocity = velocity.lerp(direction * 100, 7 * delta)
+	# speed is 50
+	velocity = velocity.lerp(direction * 50, 7 * delta)
 	
 	move_and_slide()
