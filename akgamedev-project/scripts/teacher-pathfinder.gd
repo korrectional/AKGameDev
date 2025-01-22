bf9c2c0c57fee8a19dcbc04e447cb26ee138c3cb
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-22 15:34:51 -0500

    Preparation for 2 teacher modes

--- akgamedev-project/scripts/teacher-pathfinder.gd
+++ akgamedev-project/scripts/teacher-pathfinder.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
+@export var mode: int = 0 # 0 = patrol, 1 = chase 
 
 func _physics_process(delta: float) -> void:
-
+	
 	var direction = Vector3()
+	
+	if mode == 0:
+		print(0)
+	if mode == 1:
+		print(1)
+		
 	nav.target_position = get_global_mouse_position()
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
